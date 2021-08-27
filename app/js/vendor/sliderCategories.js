5125fafa0c12e4aef3e20077738e1b866e0c967a
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-08-27 12:01:53 +0300

    page article

--- app/js/vendor/sliderCategories.js
+++ app/js/vendor/sliderCategories.js
@@ -1,16 +1,20 @@
-const sliderCategories = document.querySelector('.slider-categories__container>.swiper-container');
-const nextSlider = new Swiper(sliderCategories, {
-  loop: true,
-  autoHeight: true,
-  navigation: {
-    nextEl: '.slide-btn-next',
-    prevEl: '.slide-btn-prev',
+window.addEventListener("DOMContentLoaded", ()=> {
+  const sliderCategories = document.querySelector('.slider-categories__container>.swiper-container');
+  if(sliderCategories){
+    const nextSlider = new Swiper(sliderCategories, {
+      loop: true,
+      autoHeight: true,
+      navigation: {
+        nextEl: '.slide-btn-next',
+        prevEl: '.slide-btn-prev',
+      }
+    });
+    
+    const prodList = document.querySelectorAll('.prod-info__list');
+    
+    prodList.forEach(item=>{
+      let heightItem = item.clientHeight;
+      heightItem == 137 ? item.style.marginBottom = 63 +'px' : item.style.marginBottom = 20 + 'px';
+    });
   }
-});
-
-const prodList = document.querySelectorAll('.prod-info__list');
-
-prodList.forEach(item=>{
-  let heightItem = item.clientHeight;
-  heightItem == 137 ? item.style.marginBottom = 63 +'px' : item.style.marginBottom = 20 + 'px';
-});
+})
